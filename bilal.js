8f9d5ad22365f10a9437a53c5fbb21128c2346a0
CodeWithBilal <bilalmubarakidris@gmail.com>
2022-06-21 23:36:30 +0530

    introduction to javascript

---
+++ bilal.js
@@ -0,0 +1,7 @@
+let para = document.getElementById('name')
+const name = "Bilal Mubarak Idris";
+const height = 7.5;
+const country = "Nigeria"
+const info = `My Name is: ${name} I have Height of ${height} and I'm from ${country}`
+
+para.innerText = info;
